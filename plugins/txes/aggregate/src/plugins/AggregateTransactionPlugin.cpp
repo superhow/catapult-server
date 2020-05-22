80b49d378536e0eb8c06c1d870cd1d55827eb71c
Jaguar0625 <jaguar0625@127.0.0.1>
2020-05-22 16:05:19 -0400

    fix plugin

--- plugins/txes/aggregate/src/plugins/AggregateTransactionPlugin.cpp
+++ plugins/txes/aggregate/src/plugins/AggregateTransactionPlugin.cpp
@@ -125,8 +125,8 @@ namespace catapult { namespace plugins {
 					// - notice that all valid cosignatories must have been observed previously as part of either
 					//   (1) sub-transaction execution or (2) composite account setup
 					// - require the cosignatories to sign the aggregate indirectly via the hash of its data
-					sub.notify(SignatureNotification(pCosignature->SignerPublicKey, pCosignature->Signature, transactionInfo.hash()));
 					sub.notify(InternalPaddingNotification(pCosignature->Cosignature_Reserved1));
+					sub.notify(SignatureNotification(pCosignature->SignerPublicKey, pCosignature->Signature, transactionInfo.hash()));
 					++pCosignature;
 				}
 			}
