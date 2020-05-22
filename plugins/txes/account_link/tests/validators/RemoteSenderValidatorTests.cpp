4bd0f646fffed26404236c4dbb1d113e63593158
Jaguar0625 <jaguar0625@127.0.0.1>
2020-05-21 20:21:54 -0400

    change Keys in transaction Notifications to Addresses

--- plugins/txes/account_link/tests/validators/RemoteSenderValidatorTests.cpp
+++ plugins/txes/account_link/tests/validators/RemoteSenderValidatorTests.cpp
@@ -1,101 +1,101 @@
-/**
-*** Copyright (c) 2016-present,
-*** Jaguar0625, gimre, BloodyRookie, Tech Bureau, Corp. All rights reserved.
-***
-*** This file is part of Catapult.
-***
-*** Catapult is free software: you can redistribute it and/or modify
-*** it under the terms of the GNU Lesser General Public License as published by
-*** the Free Software Foundation, either version 3 of the License, or
-*** (at your option) any later version.
-***
-*** Catapult is distributed in the hope that it will be useful,
-*** but WITHOUT ANY WARRANTY; without even the implied warranty of
-*** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-*** GNU Lesser General Public License for more details.
-***
-*** You should have received a copy of the GNU Lesser General Public License
-*** along with Catapult. If not, see <http://www.gnu.org/licenses/>.
-**/
+// /**
+// *** Copyright (c) 2016-present,
+// *** Jaguar0625, gimre, BloodyRookie, Tech Bureau, Corp. All rights reserved.
+// ***
+// *** This file is part of Catapult.
+// ***
+// *** Catapult is free software: you can redistribute it and/or modify
+// *** it under the terms of the GNU Lesser General Public License as published by
+// *** the Free Software Foundation, either version 3 of the License, or
+// *** (at your option) any later version.
+// ***
+// *** Catapult is distributed in the hope that it will be useful,
+// *** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// *** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// *** GNU Lesser General Public License for more details.
+// ***
+// *** You should have received a copy of the GNU Lesser General Public License
+// *** along with Catapult. If not, see <http://www.gnu.org/licenses/>.
+// **/
 
-#include "src/validators/Validators.h"
-#include "catapult/cache_core/AccountStateCache.h"
-#include "catapult/model/BlockChainConfiguration.h"
-#include "tests/test/cache/CacheTestUtils.h"
-#include "tests/test/plugins/ValidatorTestUtils.h"
-#include "tests/TestHarness.h"
+// #include "src/validators/Validators.h"
+// #include "catapult/cache_core/AccountStateCache.h"
+// #include "catapult/model/BlockChainConfiguration.h"
+// #include "tests/test/cache/CacheTestUtils.h"
+// #include "tests/test/plugins/ValidatorTestUtils.h"
+// #include "tests/TestHarness.h"
 
-namespace catapult { namespace validators {
+// namespace catapult { namespace validators {
 
-#define TEST_CLASS RemoteSenderValidatorTests
+// #define TEST_CLASS RemoteSenderValidatorTests
 
-	DEFINE_COMMON_VALIDATOR_TESTS(RemoteSender,)
+// 	DEFINE_COMMON_VALIDATOR_TESTS(RemoteSender,)
 
-	namespace {
-		void AddAccount(cache::CatapultCache& cache, const Key& accountPublicKey, state::AccountType accountType) {
-			auto cacheDelta = cache.createDelta();
-			auto& accountStateCacheDelta = cacheDelta.sub<cache::AccountStateCache>();
+// 	namespace {
+// 		void AddAccount(cache::CatapultCache& cache, const Key& accountPublicKey, state::AccountType accountType) {
+// 			auto cacheDelta = cache.createDelta();
+// 			auto& accountStateCacheDelta = cacheDelta.sub<cache::AccountStateCache>();
 
-			accountStateCacheDelta.addAccount(accountPublicKey, Height(1));
-			auto accountStateIter = accountStateCacheDelta.find(accountPublicKey);
-			auto& accountState = accountStateIter.get();
-			accountState.AccountType = accountType;
+// 			accountStateCacheDelta.addAccount(accountPublicKey, Height(1));
+// 			auto accountStateIter = accountStateCacheDelta.find(accountPublicKey);
+// 			auto& accountState = accountStateIter.get();
+// 			accountState.AccountType = accountType;
 
-			cache.commit(Height(1));
-		}
+// 			cache.commit(Height(1));
+// 		}
 
-		void AssertValidation(
-				ValidationResult expectedResult,
-				const Key& accountPublicKey,
-				state::AccountType accountType,
-				const Key& notificationPublicKey) {
-			// Arrange:
-			auto cache = test::CoreSystemCacheFactory::Create(model::BlockChainConfiguration::Uninitialized());
-			AddAccount(cache, accountPublicKey, accountType);
+// 		void AssertValidation(
+// 				ValidationResult expectedResult,
+// 				const Key& accountPublicKey,
+// 				state::AccountType accountType,
+// 				const Key& notificationPublicKey) {
+// 			// Arrange:
+// 			auto cache = test::CoreSystemCacheFactory::Create(model::BlockChainConfiguration::Uninitialized());
+// 			AddAccount(cache, accountPublicKey, accountType);
 
-			auto pValidator = CreateRemoteSenderValidator();
-			auto entityType = static_cast<model::EntityType>(0x4201);
-			auto notification = model::TransactionNotification(notificationPublicKey, Hash256(), entityType, Timestamp());
+// 			auto pValidator = CreateRemoteSenderValidator();
+// 			auto entityType = static_cast<model::EntityType>(0x4201);
+// 			auto notification = model::TransactionNotification(notificationPublicKey, Hash256(), entityType, Timestamp());
 
-			// Act:
-			auto result = test::ValidateNotification(*pValidator, notification, cache);
+// 			// Act:
+// 			auto result = test::ValidateNotification(*pValidator, notification, cache);
 
-			// Assert:
-			EXPECT_EQ(expectedResult, result);
-		}
-	}
+// 			// Assert:
+// 			EXPECT_EQ(expectedResult, result);
+// 		}
+// 	}
 
-	TEST(TEST_CLASS, FailureWhenAccountIsRemoteAndSender) {
-		// Arrange:
-		auto accountPublicKey = test::GenerateRandomByteArray<Key>();
+// 	TEST(TEST_CLASS, FailureWhenAccountIsRemoteAndSender) {
+// 		// Arrange:
+// 		auto accountPublicKey = test::GenerateRandomByteArray<Key>();
 
-		// Assert:
-		constexpr auto Failure_Result = Failure_AccountLink_Remote_Account_Signer_Prohibited;
-		AssertValidation(Failure_Result, accountPublicKey, state::AccountType::Remote, accountPublicKey);
-	}
+// 		// Assert:
+// 		constexpr auto Failure_Result = Failure_AccountLink_Remote_Account_Signer_Prohibited;
+// 		AssertValidation(Failure_Result, accountPublicKey, state::AccountType::Remote, accountPublicKey);
+// 	}
 
-	TEST(TEST_CLASS, SuccessWhenSenderIsUnknown) {
-		// Arrange:
-		auto accountPublicKey = test::GenerateRandomByteArray<Key>();
-		auto notificationPublicKey = test::GenerateRandomByteArray<Key>();
+// 	TEST(TEST_CLASS, SuccessWhenSenderIsUnknown) {
+// 		// Arrange:
+// 		auto accountPublicKey = test::GenerateRandomByteArray<Key>();
+// 		auto notificationPublicKey = test::GenerateRandomByteArray<Key>();
 
-		// Assert:
-		AssertValidation(ValidationResult::Success, accountPublicKey, state::AccountType::Remote, notificationPublicKey);
-	}
+// 		// Assert:
+// 		AssertValidation(ValidationResult::Success, accountPublicKey, state::AccountType::Remote, notificationPublicKey);
+// 	}
 
-	TEST(TEST_CLASS, SuccessWhenAccountIsMainAndSender) {
-		// Arrange:
-		auto accountPublicKey = test::GenerateRandomByteArray<Key>();
+// 	TEST(TEST_CLASS, SuccessWhenAccountIsMainAndSender) {
+// 		// Arrange:
+// 		auto accountPublicKey = test::GenerateRandomByteArray<Key>();
 
-		// Assert:
-		AssertValidation(ValidationResult::Success, accountPublicKey, state::AccountType::Main, accountPublicKey);
-	}
+// 		// Assert:
+// 		AssertValidation(ValidationResult::Success, accountPublicKey, state::AccountType::Main, accountPublicKey);
+// 	}
 
-	TEST(TEST_CLASS, SuccessWhenAccountIsUnlinkedAndSender) {
-		// Arrange:
-		auto accountPublicKey = test::GenerateRandomByteArray<Key>();
+// 	TEST(TEST_CLASS, SuccessWhenAccountIsUnlinkedAndSender) {
+// 		// Arrange:
+// 		auto accountPublicKey = test::GenerateRandomByteArray<Key>();
 
-		// Assert:
-		AssertValidation(ValidationResult::Success, accountPublicKey, state::AccountType::Unlinked, accountPublicKey);
-	}
-}}
+// 		// Assert:
+// 		AssertValidation(ValidationResult::Success, accountPublicKey, state::AccountType::Unlinked, accountPublicKey);
+// 	}
+// }}
--- tests/catapult/model/TransactionPluginFactoryTests.cpp
+++ tests/catapult/model/TransactionPluginFactoryTests.cpp
@@ -1,143 +1,143 @@
-/**
-*** Copyright (c) 2016-present,
-*** Jaguar0625, gimre, BloodyRookie, Tech Bureau, Corp. All rights reserved.
-***
-*** This file is part of Catapult.
-***
-*** Catapult is free software: you can redistribute it and/or modify
-*** it under the terms of the GNU Lesser General Public License as published by
-*** the Free Software Foundation, either version 3 of the License, or
-*** (at your option) any later version.
-***
-*** Catapult is distributed in the hope that it will be useful,
-*** but WITHOUT ANY WARRANTY; without even the implied warranty of
-*** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-*** GNU Lesser General Public License for more details.
-***
-*** You should have received a copy of the GNU Lesser General Public License
-*** along with Catapult. If not, see <http://www.gnu.org/licenses/>.
-**/
+// /**
+// *** Copyright (c) 2016-present,
+// *** Jaguar0625, gimre, BloodyRookie, Tech Bureau, Corp. All rights reserved.
+// ***
+// *** This file is part of Catapult.
+// ***
+// *** Catapult is free software: you can redistribute it and/or modify
+// *** it under the terms of the GNU Lesser General Public License as published by
+// *** the Free Software Foundation, either version 3 of the License, or
+// *** (at your option) any later version.
+// ***
+// *** Catapult is distributed in the hope that it will be useful,
+// *** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// *** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// *** GNU Lesser General Public License for more details.
+// ***
+// *** You should have received a copy of the GNU Lesser General Public License
+// *** along with Catapult. If not, see <http://www.gnu.org/licenses/>.
+// **/
 
-#include "catapult/model/TransactionPluginFactory.h"
-#include "tests/test/core/mocks/MockNotificationSubscriber.h"
-#include "tests/test/core/mocks/MockTransaction.h"
-#include "tests/test/plugins/TransactionPluginTestUtils.h"
-#include "tests/TestHarness.h"
+// #include "catapult/model/TransactionPluginFactory.h"
+// #include "tests/test/core/mocks/MockNotificationSubscriber.h"
+// #include "tests/test/core/mocks/MockTransaction.h"
+// #include "tests/test/plugins/TransactionPluginTestUtils.h"
+// #include "tests/TestHarness.h"
 
-namespace catapult { namespace model {
+// namespace catapult { namespace model {
 
-#define TEST_CLASS TransactionPluginFactoryTests
+// #define TEST_CLASS TransactionPluginFactoryTests
 
-	namespace {
-		constexpr auto Mock_Transaction_Type = static_cast<EntityType>(0x4FFF);
+// 	namespace {
+// 		constexpr auto Mock_Transaction_Type = static_cast<EntityType>(0x4FFF);
 
-		template<typename TTransaction>
-		void Publish(const TTransaction& transaction, const PublishContext& context, NotificationSubscriber& sub) {
-			// raise notifications dependent on the transaction and context data
-			sub.notify(model::TransactionNotification(transaction.SignerPublicKey, Hash256(), EntityType(), Timestamp()));
-			sub.notify(mocks::MockAddressNotification(context.SignerAddress));
-		}
+// 		template<typename TTransaction>
+// 		void Publish(const TTransaction& transaction, const PublishContext& context, NotificationSubscriber& sub) {
+// 			// raise notifications dependent on the transaction and context data
+// 			sub.notify(model::TransactionNotification(transaction.SignerPublicKey, Hash256(), EntityType(), Timestamp()));
+// 			sub.notify(mocks::MockAddressNotification(context.SignerAddress));
+// 		}
 
-		template<TransactionPluginFactoryOptions Options>
-		struct RegularTraits {
-			using TransactionType = mocks::MockTransaction;
-			static constexpr auto Min_Supported_Version = TransactionType::Current_Version;
-			static constexpr auto Max_Supported_Version = TransactionType::Current_Version;
+// 		template<TransactionPluginFactoryOptions Options>
+// 		struct RegularTraits {
+// 			using TransactionType = mocks::MockTransaction;
+// 			static constexpr auto Min_Supported_Version = TransactionType::Current_Version;
+// 			static constexpr auto Max_Supported_Version = TransactionType::Current_Version;
 
-			static auto CreatePlugin() {
-				return TransactionPluginFactory<Options>::template Create<mocks::MockTransaction, mocks::EmbeddedMockTransaction>(
-						Publish<mocks::MockTransaction>,
-						Publish<mocks::EmbeddedMockTransaction>);
-			}
-		};
+// 			static auto CreatePlugin() {
+// 				return TransactionPluginFactory<Options>::template Create<mocks::MockTransaction, mocks::EmbeddedMockTransaction>(
+// 						Publish<mocks::MockTransaction>,
+// 						Publish<mocks::EmbeddedMockTransaction>);
+// 			}
+// 		};
 
-		using DefaultRegularTraits = RegularTraits<TransactionPluginFactoryOptions::Default>;
-		using OnlyEmbeddableRegularTraits = RegularTraits<TransactionPluginFactoryOptions::Only_Embeddable>;
+// 		using DefaultRegularTraits = RegularTraits<TransactionPluginFactoryOptions::Default>;
+// 		using OnlyEmbeddableRegularTraits = RegularTraits<TransactionPluginFactoryOptions::Only_Embeddable>;
 
-		template<TransactionPluginFactoryOptions Options>
-		struct EmbeddedTraits {
-			using TransactionType = mocks::EmbeddedMockTransaction;
-			static constexpr auto Min_Supported_Version = TransactionType::Current_Version;
-			static constexpr auto Max_Supported_Version = TransactionType::Current_Version;
+// 		template<TransactionPluginFactoryOptions Options>
+// 		struct EmbeddedTraits {
+// 			using TransactionType = mocks::EmbeddedMockTransaction;
+// 			static constexpr auto Min_Supported_Version = TransactionType::Current_Version;
+// 			static constexpr auto Max_Supported_Version = TransactionType::Current_Version;
 
-			static auto CreatePlugin() {
-				return TransactionPluginFactory<Options>::template CreateEmbedded<mocks::EmbeddedMockTransaction>(
-						Publish<mocks::EmbeddedMockTransaction>);
-			}
-		};
+// 			static auto CreatePlugin() {
+// 				return TransactionPluginFactory<Options>::template CreateEmbedded<mocks::EmbeddedMockTransaction>(
+// 						Publish<mocks::EmbeddedMockTransaction>);
+// 			}
+// 		};
 
-		using DefaultEmbeddedTraits = EmbeddedTraits<TransactionPluginFactoryOptions::Default>;
-		using OnlyEmbeddableEmbeddedTraits = EmbeddedTraits<TransactionPluginFactoryOptions::Only_Embeddable>;
-	}
+// 		using DefaultEmbeddedTraits = EmbeddedTraits<TransactionPluginFactoryOptions::Default>;
+// 		using OnlyEmbeddableEmbeddedTraits = EmbeddedTraits<TransactionPluginFactoryOptions::Only_Embeddable>;
+// 	}
 
-	DEFINE_BASIC_EMBEDDABLE_TRANSACTION_PLUGIN_TESTS(TEST_CLASS, Default, _Default, Mock_Transaction_Type)
-	DEFINE_BASIC_EMBEDDABLE_TRANSACTION_PLUGIN_TESTS_ONLY_EMBEDDABLE(TEST_CLASS, OnlyEmbeddable, _OnlyEmbeddable, Mock_Transaction_Type)
+// 	DEFINE_BASIC_EMBEDDABLE_TRANSACTION_PLUGIN_TESTS(TEST_CLASS, Default, _Default, Mock_Transaction_Type)
+// 	DEFINE_BASIC_EMBEDDABLE_TRANSACTION_PLUGIN_TESTS_ONLY_EMBEDDABLE(TEST_CLASS, OnlyEmbeddable, _OnlyEmbeddable, Mock_Transaction_Type)
 
-	namespace {
-		template<typename TTraits, typename TSubscriber, typename TCheckPublish>
-		void RunPublishTest(TCheckPublish checkPublish) {
-			// Arrange:
-			auto pPlugin = TTraits::CreatePlugin();
+// 	namespace {
+// 		template<typename TTraits, typename TSubscriber, typename TCheckPublish>
+// 		void RunPublishTest(TCheckPublish checkPublish) {
+// 			// Arrange:
+// 			auto pPlugin = TTraits::CreatePlugin();
 
-			typename TTraits::TransactionType transaction;
-			test::FillWithRandomData(transaction);
+// 			typename TTraits::TransactionType transaction;
+// 			test::FillWithRandomData(transaction);
 
-			PublishContext context;
-			test::FillWithRandomData(context.SignerAddress);
+// 			PublishContext context;
+// 			test::FillWithRandomData(context.SignerAddress);
 
-			TSubscriber sub;
+// 			TSubscriber sub;
 
-			// Act:
-			pPlugin->publish(transaction, context, sub);
+// 			// Act:
+// 			pPlugin->publish(transaction, context, sub);
 
-			// Assert:
-			ASSERT_EQ(2u, sub.numNotifications());
-			checkPublish(transaction, context, sub);
-		}
-	}
+// 			// Assert:
+// 			ASSERT_EQ(2u, sub.numNotifications());
+// 			checkPublish(transaction, context, sub);
+// 		}
+// 	}
 
-	PLUGIN_TEST_WITH_PREFIXED_TRAITS(CanPublishAllNotifications, Default, _Default) {
-		// Act:
-		using Subscriber = mocks::MockNotificationSubscriber;
-		RunPublishTest<TTraits, Subscriber>([](const auto&, const auto&, const auto& sub) {
-			// Assert:
-			EXPECT_EQ(TransactionNotification::Notification_Type, sub.notificationTypes()[0]);
-			EXPECT_EQ(mocks::MockAddressNotification::Notification_Type, sub.notificationTypes()[1]);
-		});
-	}
+// 	PLUGIN_TEST_WITH_PREFIXED_TRAITS(CanPublishAllNotifications, Default, _Default) {
+// 		// Act:
+// 		using Subscriber = mocks::MockNotificationSubscriber;
+// 		RunPublishTest<TTraits, Subscriber>([](const auto&, const auto&, const auto& sub) {
+// 			// Assert:
+// 			EXPECT_EQ(TransactionNotification::Notification_Type, sub.notificationTypes()[0]);
+// 			EXPECT_EQ(mocks::MockAddressNotification::Notification_Type, sub.notificationTypes()[1]);
+// 		});
+// 	}
 
-	PLUGIN_TEST_WITH_PREFIXED_TRAITS(CanPublishTransactionDependentNotifications, Default, _Default) {
-		// Act:
-		using Subscriber = mocks::MockTypedNotificationSubscriber<TransactionNotification>;
-		RunPublishTest<TTraits, Subscriber>([](const auto& transaction, const auto&, const auto& sub) {
-			// Assert:
-			ASSERT_EQ(1u, sub.numMatchingNotifications());
-			EXPECT_EQ(transaction.SignerPublicKey, sub.matchingNotifications()[0].Sender);
-		});
-	}
+// 	PLUGIN_TEST_WITH_PREFIXED_TRAITS(CanPublishTransactionDependentNotifications, Default, _Default) {
+// 		// Act:
+// 		using Subscriber = mocks::MockTypedNotificationSubscriber<TransactionNotification>;
+// 		RunPublishTest<TTraits, Subscriber>([](const auto& transaction, const auto&, const auto& sub) {
+// 			// Assert:
+// 			ASSERT_EQ(1u, sub.numMatchingNotifications());
+// 			EXPECT_EQ(transaction.SignerPublicKey, sub.matchingNotifications()[0].Sender);
+// 		});
+// 	}
 
-	PLUGIN_TEST_WITH_PREFIXED_TRAITS(CanPublishContextDependentNotifications, Default, _Default) {
-		// Act:
-		using Subscriber = mocks::MockTypedNotificationSubscriber<mocks::MockAddressNotification>;
-		RunPublishTest<TTraits, Subscriber>([](const auto&, const auto& context, const auto& sub) {
-			// Assert:
-			ASSERT_EQ(1u, sub.numMatchingNotifications());
-			EXPECT_EQ(context.SignerAddress, sub.matchingNotifications()[0].Address);
-		});
-	}
+// 	PLUGIN_TEST_WITH_PREFIXED_TRAITS(CanPublishContextDependentNotifications, Default, _Default) {
+// 		// Act:
+// 		using Subscriber = mocks::MockTypedNotificationSubscriber<mocks::MockAddressNotification>;
+// 		RunPublishTest<TTraits, Subscriber>([](const auto&, const auto& context, const auto& sub) {
+// 			// Assert:
+// 			ASSERT_EQ(1u, sub.numMatchingNotifications());
+// 			EXPECT_EQ(context.SignerAddress, sub.matchingNotifications()[0].Address);
+// 		});
+// 	}
 
-	TEST(TEST_CLASS, PluginExposesCustomAdditionalRequiredCosignatories_OnlyEmbeddable) {
-		// Arrange:
-		auto pPlugin = OnlyEmbeddableEmbeddedTraits::CreatePlugin();
+// 	TEST(TEST_CLASS, PluginExposesCustomAdditionalRequiredCosignatories_OnlyEmbeddable) {
+// 		// Arrange:
+// 		auto pPlugin = OnlyEmbeddableEmbeddedTraits::CreatePlugin();
 
-		OnlyEmbeddableEmbeddedTraits::TransactionType transaction;
-		test::FillWithRandomData(transaction);
+// 		OnlyEmbeddableEmbeddedTraits::TransactionType transaction;
+// 		test::FillWithRandomData(transaction);
 
-		// Act:
-		auto additionalCosignatories = pPlugin->additionalRequiredCosignatories(transaction);
+// 		// Act:
+// 		auto additionalCosignatories = pPlugin->additionalRequiredCosignatories(transaction);
 
-		// Assert:
-		AddressSet expectedAdditionalCosignatories{ Address{ { 1 } }, Address{ { 2 } }, mocks::GetRecipientAddress(transaction) };
-		EXPECT_EQ(expectedAdditionalCosignatories, additionalCosignatories);
-	}
-}}
+// 		// Assert:
+// 		AddressSet expectedAdditionalCosignatories{ Address{ { 1 } }, Address{ { 2 } }, mocks::GetRecipientAddress(transaction) };
+// 		EXPECT_EQ(expectedAdditionalCosignatories, additionalCosignatories);
+// 	}
+// }}
